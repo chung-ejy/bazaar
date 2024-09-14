3b5b8773ba50c2373743bb592246681de5f9c665
chung-ejy <chung.ejy@gmail.com>
2024-09-14 14:17:56 -0400

    single player

--- main_client.py
+++ main_client.py
@@ -3,6 +3,10 @@ from client.client import Client
 if __name__ == "__main__":   
     client = Client()
     client.connect()
-    message = input().strip()
-    client.send_request_to_server(message)
+    while True:
+        try:
+            message = input().strip()
+            client.send_request_to_server(message)
+        except Exception as e:
+            print(str(e))
     client.disconnect()
