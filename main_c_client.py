9c94004931b8127f723ec6ea43a0c75594f650cb
Eli <elipinals12@gmail.com>
2024-09-19 14:10:09 -0400

    tcp work

--- main_c_client.py
+++ main_c_client.py
@@ -1,14 +1,14 @@
 from client.client import Client
 import sys
 if __name__ == "__main__":   
-    client = Client("127.0.0.1","65432")
+    client = Client("tcp://127.0.0.1","65432")
     client.connect()
-    while True:
-        try:
-            message = input()
-            if message == "exit":
-                sys.exit()
-            client.send(message)
-        except Exception as e:
-            print(str(e))
+    # while True:
+    try:
+        message = input()
+        if message == "exit":
+            sys.exit()
+        client.send(message)
+    except Exception as e:
+        print(str(e))
     client.disconnect()
